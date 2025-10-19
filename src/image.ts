40fea483297b7a0a673246c04fa66bcc806230fd
Talmage <talmage.bergeson@gmail.com>
2025-10-19 11:28:20 -0600

    clean: format

--- src/image.ts
+++ src/image.ts
@@ -1,9 +1,9 @@
 export type Image = {
-	id: string;
-	artwork_id: string;
-	is_main_image: boolean;
-	image_url: string;
-	image_width: number | null;
-	image_height: number | null;
-	created_at: string;
+  id: string;
+  artwork_id: string;
+  is_main_image: boolean;
+  image_url: string;
+  image_width: number | null;
+  image_height: number | null;
+  created_at: string;
 };
